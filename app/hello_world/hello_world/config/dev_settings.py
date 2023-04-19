cb224535efa6c7ceaece3107b3910300a3ea85e8
Moez Missaoui <moeiz.missaoui@gmail.com>
2023-04-20 00:59:33 +0100

    prod && dev settings files

---
+++ app/hello_world/hello_world/config/dev_settings.py
@@ -0,0 +1,10 @@
+from .base_settings import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^ux#orw3p^5g=90zq@c46i*_+z(s)+s+ziah@briyee+fhoevu'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
