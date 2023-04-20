84be23da5b7852fac0870a15fa32be6e8555e212
Moez Missaoui <moeiz.missaoui@gmail.com>
2023-04-20 01:39:57 +0100

    initialize app wit super user

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 cd hello_world
+python manage.py flush --no-input
+python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('moez', 'moez@example.com', 'moez')" | python manage.py shell
 python manage.py runserver 0.0.0.0:8000
+
+exec "$@"
